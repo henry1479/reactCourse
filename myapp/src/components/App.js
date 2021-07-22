31cf1c394646bc711e99de3deffbbba05139dea7
henry1479 <fokys.81@mail.ru>
2021-07-22 16:00:58 +0300

    lesson-2.1

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -10,50 +10,46 @@ import { useState, useEffect } from 'react';
 function App() {
 
   // init state and function-setter
-  const[messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
 
-  //function handler on click button 'send' in the Form
-  //it get argument - event
-  const sendMessage = (event) => {
-    //an empty object for data of a message
-    let value = {};
+  //function handler onSubmit Form
+  //it get argument - object
+  const sendMessage = (object) => {
     // array is the last value of the state
     let array = messages;
-    //get values of inputs in the Form
-    // and write them in object
-    value.author = event.target.previousSibling.previousSibling.previousSibling.value;
-    value.text = event.target.previousSibling.value;
-    // adding object in the array
-    array.push(value)
+    // adding a copy of the object in the array
+    object = {...object}
+    array.push(object)
     // set the state
     setMessages([...array]);
-    // clean out inputs
-    event.target.previousSibling.previousSibling.previousSibling.value= '';
-    event.target.previousSibling.value= '';
-    
   }
 
   //send message accepting getting message 
-  useEffect(()=>{
+  useEffect(() => {
     //get a last object in the state
-    let obj = messages[messages.length-1];
-    const myLog = /K|k|ostya/;
+    let obj = messages[messages.length - 1];
+    let array = messages;
+    //my login
+    const myLog = /^(Kostya)$/i;
     // if the object do not exist to create new object 
-    obj = (obj === undefined || obj === null)? {author: 'kostya', message: ''} : obj;
+    obj = (obj === undefined || obj === null) ? { author: ' ', message: '' } : obj;
+    //if I send the meesage I will get message about ok!
+    if (myLog.test(obj.author)) {
+      const responseMessage = { author: 'robot', text: `Dear ${obj.author}, your message is sent!` }
+      array.push(responseMessage)
+      const messageIntervalId = setTimeout(() => { setMessages([...array]) }, 3000);
 
-    if(!myLog.test(obj.author)) {
-      const messageIntervalId = setTimeout(() => {console.log(`Dear ${obj.author}, your message had got!`)}, 3000);
     }
-  })
+  }, [messages])
 
- 
+
 
   return (
     <div className="App">
       <header className="App-header">
-          <MessageList data = {messages} />
-          <Form  handleChange ={sendMessage} />
+        <MessageList data={messages} />
+        <Form handleChange={sendMessage} />
       </header>
     </div>
   );
--- myapp/src/components/Form.js
+++ myapp/src/components/Form.js
@@ -1,17 +1,45 @@
-
+import { useState, useRef} from 'react'
 // component realize the form for sending message 
 // contain 2 inputs and button with handler of events
 // 
 // 
 function Form(props) {
+   // object for new message
+   const[messageObject, setMessageObject] = useState({});
+   const inputAuthor = useRef(null);
+   const inputText = useRef(null);
+
+   //get value of inputs
+   // make they propreties of objectMessage
+   const handleChange = (event) => {
+      
+      let object = messageObject;
+      const target = event.target;
+      const name = target.name;
+      const value = target.value;
+
+      if (name === 'author') {
+         object.author = value;
+      } else {
+         object.text = value;
+      }
+
+      setMessageObject(object);
+   }
+
+   //clean inputs
+   const cleanField = () => {
+      inputAuthor.current.value='';
+      inputText.current.value='';
+   }
 
   return (
-     <form action="#" className="message-form">
+     <form action="#" className="message-form" onSubmit={(event) => {event.preventDefault(); props.handleChange(messageObject);cleanField()}}>
          <label htmlFor="author">enter your name</label>
-         <input type="text" name="author"  className="message-form__author-text" />
+         <input ref={inputAuthor}type="text" name="author"  className="message-form__author-text" onChange={handleChange} />
          <label htmlFor="message">write your message</label>
-         <input type="text" name="message"  className="message-form__message-text" />
-        <button type="button" className="message-form__btn" onClick={props.handleChange} >Send</button>
+         <input ref={inputText} type="text" name="message"  className="message-form__message-text" onChange={handleChange} />
+         <input type="submit" className="message-form__btn" value="Send"/>
      </form>
   )
 }
